b90b62f66bfaaee083bb82d0b6c7ebc5bf9e76ee
Michael Cronin <michaelc2468@gmail.com>
2022-11-10 19:48:46 +0000

    added sample path to visualiztion tool

---
+++ src/test/java/display/VisualizationToolTest.java
@@ -0,0 +1,95 @@
+package display;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VisualizationToolTest {
+    @Test
+    void  VisualizationTool(){
+
+    }
+    @Test
+    void paintComponent() {
+    }
+
+    @Test
+    void getStep() {
+    }
+
+    @Test
+    void setStep() {
+    }
+
+    @Test
+    void getSpacing() {
+    }
+
+    @Test
+    void setSpacing() {
+    }
+
+    @Test
+    void getBoxSize() {
+    }
+
+    @Test
+    void setBoxSize() {
+    }
+
+    @Test
+    void getTimer() {
+    }
+
+    @Test
+    void setTimer() {
+    }
+
+    @Test
+    void getPath() {
+    }
+
+    @Test
+    void setPath() {
+    }
+
+    @Test
+    void getMap() {
+    }
+
+    @Test
+    void setMap() {
+    }
+
+    @Test
+    void getMapHeight() {
+    }
+
+    @Test
+    void setMapHeight() {
+    }
+
+    @Test
+    void getMapWidth() {
+    }
+
+    @Test
+    void setMapWidth() {
+    }
+
+    @Test
+    void getChargingAreaLocation() {
+    }
+
+    @Test
+    void setChargingAreaLocation() {
+    }
+
+    @Test
+    void getDispatchAreaLocation() {
+    }
+
+    @Test
+    void setDispatchAreaLocation() {
+    }
+}
