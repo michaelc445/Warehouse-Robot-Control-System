5929f5d28d5921e73bd782e887152667dad0c383
Konrad-Dagiel <kdawgy9@gmail.com>
2022-11-13 22:44:41 +0000

    added functionality to buttons in order system

--- src/main/java/main/OrderSystem.java
+++ src/main/java/main/OrderSystem.java
@@ -11,54 +11,54 @@ import java.util.Set;
 
 public class OrderSystem extends JFrame implements ActionListener {
      JComboBox<Object> comboBox;
-     JButton addItemToWarehouseBtn;
-     JButton removeItemFromWarehouseBtn;
+     JButton addToOrderBtn;
+     JButton removeFromOrderBtn;
      Set<Point> itemLocationsToVisit;
      Warehouse warehouse = new Warehouse();
      OrderSystem(){
          warehouse.addItem("Hammer", new Point(1,1));
          warehouse.addItem("Screw", new Point(2,2));
          warehouse.addItem("Helmet", new Point(3,3));
+         warehouse.addItem("Axe", new Point(3,1));
+         warehouse.addItem("Wrench", new Point(2,3));
 
          Object[] itemsInStock = warehouse.getItemNames();
          itemLocationsToVisit = new HashSet<>();
-         addItemToWarehouseBtn = new JButton();
-         addItemToWarehouseBtn.setBounds(200,100,100,50);
-         addItemToWarehouseBtn.setText("Add to Warehouse");
-         addItemToWarehouseBtn.setFocusable(false);
-         removeItemFromWarehouseBtn = new JButton();
-         removeItemFromWarehouseBtn.setBounds(200,100,100,50);
-         removeItemFromWarehouseBtn.setText("Remove from Warehouse");
-         removeItemFromWarehouseBtn.setFocusable(false);
+         addToOrderBtn = new JButton();
+         addToOrderBtn.setBounds(200,100,100,50);
+         addToOrderBtn.setText("Add to Order");
+         addToOrderBtn.setFocusable(false);
+         removeFromOrderBtn = new JButton();
+         removeFromOrderBtn.setBounds(200,100,100,50);
+         removeFromOrderBtn.setText("Remove from Order");
+         removeFromOrderBtn.setFocusable(false);
 
          comboBox = new JComboBox<>(itemsInStock);
 
          this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
          this.setLayout(new FlowLayout());
          this.setVisible(true);
-         this.add(addItemToWarehouseBtn);
-         this.add(removeItemFromWarehouseBtn);
+         this.add(addToOrderBtn);
+         this.add(removeFromOrderBtn);
          this.add(comboBox);
 
          this.pack();
 
-         addItemToWarehouseBtn.addActionListener(this);
-         removeItemFromWarehouseBtn.addActionListener(this);
+         addToOrderBtn.addActionListener(this);
+         removeFromOrderBtn.addActionListener(this);
          comboBox.addActionListener(this);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == comboBox){
-            String selectedItem = (String) comboBox.getSelectedItem();
+        String selectedItem = (String) comboBox.getSelectedItem();
+        if (e.getSource() == addToOrderBtn) {
             itemLocationsToVisit.add(warehouse.getItemLocation(selectedItem));
             System.out.println(itemLocationsToVisit);
         }
-        else if (e.getSource() == addItemToWarehouseBtn) {
-            System.out.println("poo");
-        }
-        else if (e.getSource() == removeItemFromWarehouseBtn) {
-            System.out.println("pee");
+        else if (e.getSource() == removeFromOrderBtn) {
+            itemLocationsToVisit.remove(warehouse.getItemLocation(selectedItem));
+            System.out.println(itemLocationsToVisit);
         }
     }
 }
