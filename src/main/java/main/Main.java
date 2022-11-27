d2e1a266acbc0ba2b4dcc3a69e4c55e4ff5a4596
KeithS <kscannell11@gmail.com>
2022-11-27 16:17:17 +0000

    added constructionItems.csv and pngs

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -10,7 +10,7 @@ import java.util.*;
 
 public class Main {
     private static final String WAREHOUSE_MAP_FILE = "testMapMedium.csv";
-    private static final String ITEM_FILE = "items.csv";
+    private static final String ITEM_FILE = "constructionItems.csv";
     private static final Point START_POINT = new Point(0, 0);
     private static final Point END_POINT = new Point(0, 6);
 
