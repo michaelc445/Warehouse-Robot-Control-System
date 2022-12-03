fdfe075ec2977766f106771778120ce052de962f
Artem Tiupin <tanerus2@gmail.com>
2022-12-03 02:22:04 +0000

    feat: add toString()

--- src/main/java/stock/ItemOrder.java
+++ src/main/java/stock/ItemOrder.java
@@ -3,4 +3,9 @@ package stock;
 import graph.Point;
 
 public record ItemOrder(String name, Point location, int quantity, int weight) {
+    @Override
+    public String toString() {
+        return  "{name='" + name.split(" ")[0] + '\'' +
+                ", quantity=" + quantity + '}';
+    }
 }
